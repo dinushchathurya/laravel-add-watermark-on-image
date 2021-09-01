8d7344a9c3813fec37be53f3500daf7bc6de8669
dinushchathurya <dinushchathurya21@gmail.com>
2021-09-01 07:30:44 +0530

    :wrench: Register Image Intervention package in app.php

--- config/app.php
+++ config/app.php
@@ -165,7 +165,7 @@ return [
         /*
          * Package Service Providers...
          */
-
+        'Intervention\Image\ImageServiceProvider',
         /*
          * Application Service Providers...
          */
@@ -228,7 +228,7 @@ return [
         'URL' => Illuminate\Support\Facades\URL::class,
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
-
+        'Image' => 'Intervention\Image\Facades\Image',
     ],
 
 ];
