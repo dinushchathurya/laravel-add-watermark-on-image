33b6eb68328422915d3c65d388ff8a4c509014d1
dinushchathurya <dinushchathurya21@gmail.com>
2021-09-01 14:24:43 +0530

    :sparkles: Create function to add watermark to uploaded image

--- app/Http/Controllers/ImageController.php
+++ app/Http/Controllers/ImageController.php
@@ -3,11 +3,35 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Image;
 
 class ImageController extends Controller
 {
     public function index()
     {
         return view('welcome');
+    }
+
+    public function imageFileUpload(Request $request)
+    {
+        $this->validate($request, [
+            'file' => 'required|image|mimes:jpg,jpeg,png,gif,svg|max:4096',
+        ]);
+
+        $image = $request->file('file');
+        $input['file'] = time().'.'.$image->getClientOriginalExtension();
+
+        $imgFile = Image::make($image->getRealPath());
+
+        $imgFile->text('© 2021 codingtricks.io', 50, 50, function($font) { 
+            $font->size(60);  
+            $font->color('#FF0000');  
+            $font->align('center');  
+            $font->align('bottom');  
+        })->save(public_path('/upload').'/'.$input['file']);          
+
+        return back()
+        	->with('success','File uploaded successfully ')
+        	->with('fileName',$input['file']);         
     }
 }
