012b328e4966af1d4089257ca6362d1fd8e06c73
dinushchathurya <dinushchathurya21@gmail.com>
2021-09-01 14:26:22 +0530

    :sparkles: Add required routes

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ImageController;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +14,5 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [ImageController::class, 'index']);
+Route::post('/add-watermark', [ImageController::class, 'imageFileUpload'])->name('image.watermark');
